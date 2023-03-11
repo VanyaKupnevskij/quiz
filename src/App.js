08b24c5cf234ac481633de47165961e0006bd80a
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-03-11 12:58:29 +0200

    start config, setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello all</h1>
-      <h2>Subtitle</h2>
-      <p>sdlru woiu j sejh iw efjoisue io hsrg e</p>
-      <p>sdlru woiu j sejh iw </p>
-      <p>sdlru se ef uiweufiefjoisue io hsrg e</p>
+    <div class="container">
+      <header>
+        <h1>_ Header _</h1>
+      </header>
+      <section class="main">
+        <div className="App">
+          <h1>Hello all</h1>
+          <h2>Subtitle</h2>
+          <p>sdlru woiu j sejh iw efjoisue io hsrg e</p>
+          <p>sdlru woiu j sejh iw </p>
+          <p>sdlru se ef uiweufiefjoisue io hsrg e</p>
+        </div>
+      </section>
     </div>
   );
 }
